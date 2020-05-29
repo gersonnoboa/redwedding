b1dfb37223ee828c8a1b37a85807b09d5366d68b
Gerson Noboa <gerson.noboa@pipedrive.com>
2020-05-30 01:10:06 +0300

    Initial Commit

---
+++ RedWedding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedWedding
+//
+//  Created by Gerson Noboa on 30.05.2020.
+//  Copyright © 2020 Gerson Noboa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
